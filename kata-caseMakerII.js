304ddf3c8e8ee13d7499479c8a49d2fd49a3ed36
Subin <jess.s.moon@gmail.com>
2020-12-07 15:10:54 -0500

    intial commit

---
+++ kata-caseMakerII.js
@@ -0,0 +1,22 @@
+const makeCase = function(input, givenCase) {
+  // Put your solution here
+}
+
+console.log(makeCase("this is a string", "camel"));
+console.log(makeCase("this is a string", "pascal"));
+console.log(makeCase("this is a string", "snake"));
+console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "vowel"));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
+
+// Expected Output
+// thisIsAString
+// ThisIsAString
+// this_is_a_string
+// this-is-a-string
+// This Is A String
+// thIs Is A strIng
+// THiS iS a STRiNG
+// THIS_IS_A_STRING
